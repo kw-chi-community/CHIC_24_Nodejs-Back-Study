8fe05cd19b0fecb2d4ff60372f61767f2a6898e1
SeongYun_Cheon <1112csy@naver.com>
2024-07-09 14:43:40 +0900

    코드 주석 추가

--- 1분반/천성윤/2주차/web2-nodejs/main2.js
+++ 1분반/천성윤/2주차/web2-nodejs/main2.js
@@ -1,9 +1,10 @@
+// 모듈 3가지를 불러온다
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-
+//서버생성
 var app = http.createServer(function (request, response) {
-    var _url = request.url;
+    var _url = request.url;//url 분석
     var queryData = url.parse(_url, true).query;
     console.log(queryData.id);
     if (_url == '/') {
@@ -17,3 +18,6 @@ var app = http.createServer(function (request, response) {
 
 });
 app.listen(3000);
+
+//URL의 쿼리 문자열에서 id 값을 추출하여 클라이언트에게 반환하는 간단한 HTTP 서버를 만듦
+// ex)  클라이언트가 http://localhost:3000/?id=1234로 요청을 보내면 서버는 1234를 응답으로 반환
